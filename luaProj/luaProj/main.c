b8555f50617e8e505bd970e9b389a2fde2420405
吴玉强 <yuqiang.wu@redatoms.com>
2014-11-06 18:04:32 +0800

    add source

---
+++ luaProj/luaProj/main.c
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  luaProj
+//
+//  Created by wyq on 14/11/6.
+//  Copyright (c) 2014年 cloudayc. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
