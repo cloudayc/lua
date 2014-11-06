f091f69925a813540fe9c5e8d66f08b2fa671e7b
吴玉强 <yuqiang.wu@redatoms.com>
2014-11-06 18:12:39 +0800

    add proj

--- luaProj/luaProj/main.c
+++ luaProj/luaProj/main.c
@@ -6,10 +6,7 @@
 //  Copyright (c) 2014年 cloudayc. All rights reserved.
 //
 
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main (int argc, char **argv)
+{
     return 0;
 }
